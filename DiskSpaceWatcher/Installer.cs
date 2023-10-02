4cf34d2e5a65d03bc4094a468eb8dc7bb724e828
TakeAsh <take@TakeAsh.net>
2023-10-03 05:35:58 +0900

    create shortcut in startup at installation

--- DiskSpaceWatcher/Installer.cs
+++ DiskSpaceWatcher/Installer.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.ComponentModel;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Security.Principal;
+using System.Windows.Forms;
 
 namespace DiskSpaceWatcher {
     public static class Installer {
         static string _pathLocal = null;
+        static string _pathStartup = null;
 
         private static string PathLocal {
             get {
@@ -18,23 +21,35 @@ namespace DiskSpaceWatcher {
             }
         }
 
+        private static string PathStartup {
+            get {
+                return _pathStartup ??
+                    (_pathStartup = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.Startup),
+                        Properties.Resources.Product + ".lnk"
+                    ));
+            }
+        }
+
         public static InstallerResult Install() {
             var isAdmin = IsAdministrator();
             Notifier.DebugShow($"isAdmin: {isAdmin}");
             if (!isAdmin) { return InstallerResult.RequireAdmin; }
             Directory.CreateDirectory(PathLocal);
-            return !Notifier.Install(PathLocal)
-                ? InstallerResult.Failure
-                : InstallerResult.Success;
+            CreateShortCut(PathStartup, Application.ExecutablePath);
+            return
+                !Notifier.Install(PathLocal) ? InstallerResult.Failure :
+                InstallerResult.Success;
         }
 
         public static InstallerResult Uninstall() {
             var isAdmin = IsAdministrator();
             Notifier.DebugShow($"isAdmin: {isAdmin}");
             if (!isAdmin) { return InstallerResult.RequireAdmin; }
-            var result = !Notifier.Uninstall(PathLocal)
-                ? InstallerResult.Failure
-                : InstallerResult.Success;
+            File.Delete(PathStartup);
+            var result =
+                !Notifier.Uninstall(PathLocal) ? InstallerResult.Failure :
+                InstallerResult.Success;
             Directory.Delete(PathLocal, true);
             return result;
         }
@@ -44,6 +59,16 @@ namespace DiskSpaceWatcher {
                 var principal = new WindowsPrincipal(identity);
                 return principal.IsInRole(WindowsBuiltInRole.Administrator);
             }
+        }
+
+        public static void CreateShortCut(string path, string target, string arguments = "") {
+            var shell = new IWshRuntimeLibrary.WshShell();
+            var shortcut = shell.CreateShortcut(path) as IWshRuntimeLibrary.IWshShortcut;
+            shortcut.TargetPath = target;
+            shortcut.Arguments = arguments;
+            shortcut.Save();
+            Marshal.FinalReleaseComObject(shortcut);
+            Marshal.FinalReleaseComObject(shell);
         }
     }
 
